cdc529eb695e99c0fad227716bb9a324a1c00d3f
Gillian <72627535+buijs-dev@users.noreply.github.com>
2023-10-13 18:02:24 +0200

    Bump compatible dependencies.

--- lib/klutter_ui.dart
+++ lib/klutter_ui.dart
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+/// Flutter widgets to be used in conjunction with the klutter plugin.
 library klutter_ui;
 
 export "src/adapter.dart";
